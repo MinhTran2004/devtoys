4755bff754eff68fe7fc5200702a779237034d1b
MinhTran2004 <tranminh209204@gmail.com>
2025-03-20 14:08:42 +0700

    feat(app): add funcion login and register auth0

---
+++ src/components/header/page.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import PrimaryButton from "../button";
+import LogoutIcon from '@mui/icons-material/Logout';
+
+interface HeaderProps{
+    nameUser?: string,
+}
+
+export default function Header({
+    nameUser
+}: HeaderProps) {
+    const handleLogout = () => {
+        window.location.href = "/api/auth/logout";
+    };
+
+    const handleLogin = () => {
+        window.location.href = "/api/auth/login";
+    };
+
+
+
+    return (
+        <div className="flex items-end gap-2">
+            <p>Hi, {nameUser}</p>
+            <PrimaryButton
+                name="Logout"
+                disabled={true}
+                iconRight={<LogoutIcon sx={{ height: 18 }} />}
+                style={{height: 10}}
+                onClick={handleLogout}
+            />
+        </div>
+    )
+}
